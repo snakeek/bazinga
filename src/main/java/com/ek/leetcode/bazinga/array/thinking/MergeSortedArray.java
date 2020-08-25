71211e7f241a36af025f809c15f07e52ea4c25a9
snakeek <skywalker124@gmail.com>
2020-08-25 21:06:29 +0800

    Create MergeSortedArray.java
    tmp

---
+++ src/main/java/com/ek/leetcode/bazinga/array/thinking/MergeSortedArray.java
@@ -0,0 +1,27 @@
+package com.ek.leetcode.bazinga.array.thinking;
+
+public class MergeSortedArray {
+
+    /***
+     * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+     * 说明:
+     *
+     * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+     * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (nums1 == null || nums1.length == 0) {
+            nums1 = nums2;
+            return;
+        }
+        if (nums2 == null || nums2.length == 0) {
+            return;
+        }
+
+
+    }
+}

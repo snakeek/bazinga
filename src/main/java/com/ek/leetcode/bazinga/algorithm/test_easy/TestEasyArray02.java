05ae95b18d6de55acf3a235bc15d0d02d3f6f02f
snakeek <skywalker124@gmail.com>
2020-08-29 19:13:45 +0800

    cache

--- src/main/java/com/ek/leetcode/bazinga/algorithm/test_easy/TestEasyArray02.java
+++ src/main/java/com/ek/leetcode/bazinga/algorithm/test_easy/TestEasyArray02.java
@@ -3,14 +3,14 @@ package com.ek.leetcode.bazinga.algorithm.test_easy;
 public class TestEasyArray02 {
     public static void main(String[] args) {
         TestEasyArray02 t = new TestEasyArray02();
-        System.out.println(t.maxProfit(new int[]{7,1,5,3,6,4}));
+        System.out.println(t.maxProfit(new int[]{7,6,4,3,1}));
     }
 
     public int maxProfit(int[] prices) {
         int index = 0;
         int earn = 0;
         int tmp = 0;
-        for (int i = 1; i < prices.length - 1; i++) {
+        for (int i = 1; i < prices.length; i++) {
             if (prices[index] > prices[i]) {
                 if (tmp > 0) {
                     earn += tmp;
@@ -26,6 +26,9 @@ public class TestEasyArray02 {
                 tmp = 0;
                 index = i;
             }
+        }
+        if (tmp > 0) {
+            earn += tmp;
         }
         return earn;
     }
---
+++ src/main/java/com/ek/leetcode/bazinga/algorithm/test_easy/TestEasyArray03.java
@@ -0,0 +1,16 @@
+package com.ek.leetcode.bazinga.algorithm.test_easy;
+
+public class TestEasyArray03 {
+    public static void main(String[] args) {
+
+    }
+
+    public void rotate(int[] nums, int k) {
+
+        for (int i = nums.length -1; i >= (nums.length - k); i--) {
+
+        }
+        int len = nums.length;
+
+    }
+}

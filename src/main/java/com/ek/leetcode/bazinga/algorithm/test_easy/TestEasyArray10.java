d14f30bb95ef6308e5f79eea395a85d5f6f12dce
renyuechao <renyuechao@gaosiedu.com>
2020-08-31 19:30:15 +0800

    add

---
+++ src/main/java/com/ek/leetcode/bazinga/algorithm/test_easy/TestEasyArray10.java
@@ -0,0 +1,28 @@
+package com.ek.leetcode.bazinga.algorithm.test_easy;
+
+/**
+ * Copyright (C), 2019-2020
+ * FileName: TestEasyArray10
+ * Author:   renyc
+ * Date:     2020/8/31 6:29 下午
+ * Description: 检查数独有效性
+ */
+public class TestEasyArray10 {
+    private static char[][] first = new char[][]{};
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isValidSudoku(char[][] board) {
+
+    }
+
+    private boolean checkHorizon() {
+
+    }
+
+    private boolean checkVertical() {
+
+    }
+}

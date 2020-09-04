b5a0366e8c5fbaa411bec0db31cd1fc4bc85a229
snakeek <skywalker124@gmail.com>
2020-09-04 23:05:11 +0800

    cache

---
+++ src/main/java/com/ek/leetcode/bazinga/algorithm/test_easy/TestEasyArray08.java
@@ -0,0 +1,18 @@
+package com.ek.leetcode.bazinga.algorithm.test_easy;
+
+/**
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ */
+public class TestEasyArray08 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void moveZeroes(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+    }
+}

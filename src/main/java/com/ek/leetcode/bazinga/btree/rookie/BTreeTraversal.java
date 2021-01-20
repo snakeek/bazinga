641f182ebf9e054000be8a909959ef2d4b1312d8
snakeek <skywalker124@gmail.com>
2021-01-21 02:21:05 +0800

    cache

--- src/main/java/com/ek/leetcode/bazinga/btree/rookie/BTreeTraversal.java
+++ src/main/java/com/ek/leetcode/bazinga/btree/rookie/BTreeTraversal.java
@@ -7,7 +7,15 @@ import java.util.List;
 
 public class BTreeTraversal {
     public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.setLeft(null);
+        root.setRight(new TreeNode(2));
+        root.getRight().setLeft(new TreeNode(3));
 
+        BTreeTraversal bTreeTraversal = new BTreeTraversal();
+        bTreeTraversal.preOrderTraversal(root).stream().forEach(System.out::println);
+        System.out.println("======");
+        bTreeTraversal.inOrderTraversal(root).stream().forEach(System.out::println);
     }
 
     /**
@@ -16,7 +24,22 @@ public class BTreeTraversal {
      */
     private List<Integer> preOrderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
+        preOrderTraversal(root, list);
         return list;
+    }
+
+    /**
+     * 前序遍历 PreOrder
+     * 根左右
+     * 递归
+     */
+    private void preOrderTraversal(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(root.getVal());
+        preOrderTraversal(root.getLeft(), list);
+        preOrderTraversal(root.getRight(), list);
     }
 
     /**
@@ -25,7 +48,17 @@ public class BTreeTraversal {
      */
     private List<Integer> inOrderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
+        inOrderTraversal(root, list);
         return list;
+    }
+
+    private void inOrderTraversal(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inOrderTraversal(root.getLeft(), list);
+        list.add(root.getVal());
+        inOrderTraversal(root.getRight(), list);
     }
 
     /**
---
+++ src/main/java/com/ek/leetcode/bazinga/interview/Baidu001.java
@@ -0,0 +1,9 @@
+package com.ek.leetcode.bazinga.interview;
+
+/**
+ * 给定数组A，长度n，值均大于等于0
+ * 下标j>=0, j<=i, i<=n
+ * 求A[i]/A[j]的最小值
+ */
+public class Baidu001 {
+}
